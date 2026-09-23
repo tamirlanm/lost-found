803daaef33127d50a2d809356994cc159df3fd23
tamirlanm <tamirlanmirgali@gmail.com>
2026-09-23 12:54:34 +0500

    changes founditem

--- src/LostAndFound.Api/Features/Items/FoundItem.cs
+++ src/LostAndFound.Api/Features/Items/FoundItem.cs
@@ -1,14 +1,39 @@
 namespace  LostAndFound.Api.Features.Items;
-
+using System;
 public class FoundItem
 {
     public Guid Id { get; private set; }
     public Guid UserFoundId { get; private set; }
     public string Name { get; private set; } = string.Empty;
     public string Description { get; private set; } = string.Empty;
-    public string Category { get; private set; } = string.Empty;
+    public ItemCategory Category { get; private set; }
     public string Cabinet { get; private set; } = string.Empty;
     public FoundItemStatus Status { get; private set; }
     public DateTimeOffset FoundStatus { get; private set; }
-    public DateTime CreatedAt { get; private set; }
+    public DateTimeOffset CreatedAt { get; private set; }
+    public string? PhotoKey {get;set;}
+
+    private FoundItem()
+    { }
+
+    public FoundItem(
+        Guid userFoundId,
+        string name,
+        string description,
+        ItemCategory category,
+        string cabinet,
+        DateTime foundStatus,
+        // DateTime createdAt,
+        string? photoKey)
+    {
+        Id = Guid.NewGuid();
+        UserFoundId = userFoundId;
+        Name = name;
+        Description = description;
+        Category = category;
+        Cabinet = cabinet;
+        FoundStatus = foundStatus.ToUniversalTime();
+        CreatedAt = DateTimeOffset.UtcNow;
+        PhotoKey = photoKey;
+    }
 }
---
+++ src/LostAndFound.Api/Features/Items/ItemCategory.cs
@@ -0,0 +1,11 @@
+namespace LostAndFound.Api.Features.Items;
+
+public enum ItemCategory
+{
+    Accessory,
+    Clothing,
+    Electronics, 
+    Documents,
+    Keys,
+    Other
+}
